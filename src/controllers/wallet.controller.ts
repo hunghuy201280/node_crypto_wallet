821faf7a2681dfe5fb4d6ae2316e804af1dc7d9d
nxhung.hit <nxhung16102001@gmail.com>
2022-05-07 17:30:04 +0700

    dev - import mnemonic phrase

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -1,11 +1,10 @@
 import Web3 from "web3";
-import logger from "../utils/logger";
 import HDWalletProvider from "@truffle/hdwallet-provider";
 import { RequestHandler } from "express";
 import * as ethers from "ethers";
-import * as bip39 from "bip39";
 import { ErrorResponse, SuccessResponse } from "../utils/base_response";
-
+import * as k from "../utils/constants";
+import * as bip39 from "bip39";
 export const importWalletFromPrivateKey: RequestHandler = async (req, res) => {
   try {
     const privateKey = req.body.privateKey;
@@ -19,62 +18,70 @@ export const importWalletFromPrivateKey: RequestHandler = async (req, res) => {
     const wallets = await web3.eth.getAccounts();
     if (wallets.length > 0) {
       res.json(
-        new SuccessResponse("success", res.statusCode, {
-          wallets,
+        new SuccessResponse("success", 201, {
+          address: wallets,
+          privateKey,
         })
       );
     } else {
       throw "Invalid secret phrase";
     }
   } catch (err: any) {
-    logger.error(err);
-    res.status(400).send(new ErrorResponse(err.toString(), res.statusCode));
-  }
-};
-
-export const getTokens: RequestHandler = async (req, res) => {
-  try {
-    const privateKey : string = req.body.privateKey;
-
-    const provider = new HDWalletProvider({
-      privateKeys: [privateKey],
-      providerOrUrl: process.env.RINKEBY,
-    });
-    const web3 = new Web3();
-    web3.setProvider(provider);
-    const wallets = await web3.eth.getAccounts();
-    if (wallets.length > 0) {
-      res.json(
-        new SuccessResponse("success", res.statusCode, {
-          wallets,
-        })
-      );
-    } else {
-      res
-        .status(400)
-        .send(new ErrorResponse("Invalid secret phrase", res.statusCode));
-    }
-  } catch (err: any) {
-    logger.error(err);
-    res.status(400).send(new ErrorResponse(err.toString(), res.statusCode));
+    console.log(err);
+    res.status(400).send(new ErrorResponse(err.toString(), 400));
   }
 };
 
 export const createWallet: RequestHandler = async (_, res) => {
   try {
     const mnemonic = bip39.generateMnemonic();
-    let mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
-    logger.info(mnemonicWallet.privateKey);
-    const privateKey = mnemonicWallet.privateKey;
-    const publicKey = mnemonicWallet.publicKey;
+    // const mnemonic =
+    //   "cause almost leaf coast ability mesh oval media marine purse parade chapter";
+
+    let wallet = ethers.Wallet.fromMnemonic(mnemonic, `${k.WALLET_PATH}1`);
+    const privateKey = wallet.privateKey;
+    const address = await wallet.getAddress();
+
     res.status(201).json(
-      new SuccessResponse("success", res.statusCode, {
-        publicKey,
-        privateKey,
+      new SuccessResponse("success", 201, {
+        mnemonic,
+        wallet: {
+          address,
+          privateKey,
+        },
       })
     );
   } catch (err: any) {
-    logger.error(err);
-    res.status(500).send(new ErrorResponse(err.toString(), res.statusCode));
+    console.log(err);
+    res.status(500).send(new ErrorResponse(err.toString(), 500));
+  }
+};
+
+export const importWalletFromMnemonic: RequestHandler = async  (req, res) => {
+  try {
+    const { mnemonic } = req.body;
+    var words = mnemonic.trim().split(" ");
+    if (words.length == 12 || words.length == 24) {
+      // validate mnemonic
+      if (!bip39.validateMnemonic(mnemonic)) {
+        throw new Error("Mnemonic invalid or undefined");
+      }
+      const wallet = ethers.Wallet.fromMnemonic(mnemonic, `${k.WALLET_PATH}1`);
+      let privateKey = wallet.privateKey;
+      let address = wallet.address;
+      return res.status(201).json(
+        new SuccessResponse("success", 201, {
+          mnemonic,
+          wallet: {
+            address,
+            privateKey,
+          },
+        })
+      );
+    } else
+      throw new Error("Mnemonic words length must be 12 or 24 words");
+  } catch (err: any) {
+    console.log(err);
+    return res.status(400).send(new ErrorResponse(err.toString(), res.statusCode));
   }
 };
--- src/routers/wallet.route.ts
+++ src/routers/wallet.route.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {
   importWalletFromPrivateKey,
+  importWalletFromMnemonic,
   createWallet,
 } from "../controllers/wallet.controller";
 
 const router = Router();
 
-router.post("/verify", importWalletFromPrivateKey);
+router.post("/verify_private", importWalletFromPrivateKey);
+router.post("/verify_mnemonic",importWalletFromMnemonic);
 router.post("/create", createWallet);
 
 export default router;
