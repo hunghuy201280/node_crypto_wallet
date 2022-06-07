ad28281aed3807342b2a6cea577b5098c41c8e0c
nxhung.hit <nxhung16102001@gmail.com>
2022-06-07 22:54:07 +0700

    update - get price amount of token

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,15 @@
 import { Token, TradeType } from "@uniswap/sdk-core";
-import * as rpsAbiJsonRaw from "../utils/rps.abi.json";
-import {
-  AlphaRouter,
-  ChainId,
-  CurrencyAmount,
-} from "@uniswap/smart-order-router";
-import JSBI from "jsbi";
+import * as bep20AbiJsonRaw from "../utils/interfaces/bep20.abi.json";
+import * as pancakeSwapFactoryAbiJsonRaw from "../utils/interfaces/pancakeFactory.abi.json";
+import * as pancakeSwapRouteAbiJsonRaw from "../utils/interfaces/pancakeRoute.abi.json";
 import Web3 from "web3";
-import { ethers } from "ethers";
 import { AbiItem } from "web3-utils";
-const rpsAbiJson = (rpsAbiJsonRaw as any).default["abi"] as AbiItem[];
+import { PANCAKESWAP_FACTORY_ADDRESS, PANCAKESWAP_ROUTE_ADDRESS } from "./constants";
+import log from "./logger";
+import { WETH } from "pancakeswap-v2-testnet-sdk";
+const bep20AbiJson = (bep20AbiJsonRaw as any).default as AbiItem[];
+const pancakeSwapFactoryAbiJson = (pancakeSwapFactoryAbiJsonRaw as any).default as AbiItem[];
+const pancakeSwapRouteAbiJson = (pancakeSwapRouteAbiJsonRaw as any).default as AbiItem[];
 export const getWeb3Instance = (): Web3 => {
   const web3 = new Web3(
     new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER_URL as string)
@@ -17,7 +17,7 @@ export const getWeb3Instance = (): Web3 => {
   return web3;
 };
 
-export const getPriceOfToken = async (address: string): Promise<number> => {
+export const getPriceOfToken = async (address: string ,balance : number): Promise<number> => {
   // const provider = new ethers.providers.JsonRpcProvider(process.env.WEB3_PROVIDER_URL as string)
   // const abisFactory = ((IUniswapV3FactoryABI as any).default)["abi"]
   // const abisPool = ((IUniswapV3PoolABI as any).default)["abi"]
@@ -28,67 +28,17 @@ export const getPriceOfToken = async (address: string): Promise<number> => {
   // var sqrtPriceX96 = poolBalance[0];
   // return sqrtPriceX96;
   try {
-    const fromTokenAddress = address;
-    const toTokenAddress = "0x3813e82e6f7098b9583FC0F33a962D02018B6803";
-    const web3 = getWeb3Instance();
-    const provider = new ethers.providers.JsonRpcProvider(
-      process.env.SUB_PROVIDER_URL as string
-    );
-
-    //#region load meta data
-    const fromTokenContract = new web3.eth.Contract(
-      rpsAbiJson,
-      fromTokenAddress
-    );
-    const toTokenContract = new web3.eth.Contract(rpsAbiJson, toTokenAddress);
-    const promiseResult = await Promise.all([
-      fromTokenContract.methods.decimals().call(),
-      toTokenContract.methods.decimals().call(),
-    ]);
-    const temp = {
-      from: {
-        decimals: parseInt(promiseResult[0]),
-        address: fromTokenAddress,
-      },
-      to: {
-        decimals: parseInt(promiseResult[1]),
-        address: toTokenAddress,
-      },
-    };
-
-    const fromToken = new Token(
-      ChainId.POLYGON_MUMBAI,
-      temp.from.address,
-      temp.from.decimals
-    );
-    const toToken = new Token(
-      ChainId.POLYGON_MUMBAI,
-      temp.to.address,
-      temp.to.decimals
-    );
-    //#endregion
-
-    //#region convert amount
-    const wei = ethers.utils.parseUnits("1", temp.from.decimals);
-    const inputAmount = CurrencyAmount.fromRawAmount(
-      fromToken,
-      JSBI.BigInt(wei)
-    );
-    //#endregion
-
-    //#region create router
-    const alphaRouter = new AlphaRouter({
-      chainId: ChainId.POLYGON_MUMBAI,
-      provider: provider,
-    });
-    const route = await alphaRouter.route(
-      inputAmount,
-      toToken,
-      TradeType.EXACT_INPUT
-    );
-    console.log(route);
-    return 0;
+    const web3 = getWeb3Instance()
+    const fromAddress = address
+    const toAddress = '0x7afd064DaE94d73ee37d19ff2D264f5A2903bBB0'
+    const pancakeSwapRoute = new web3.eth.Contract(pancakeSwapRouteAbiJson,PANCAKESWAP_ROUTE_ADDRESS) 
+    if(fromAddress == toAddress) {
+      return balance;
+    }
+    const amountOut = await pancakeSwapRoute.methods.getAmountsOut(web3.utils.toWei("1", "ether"), [fromAddress,toAddress]).call();
+    return Number(web3.utils.fromWei(amountOut[1])) 
   } catch (e: any) {
+    log.error(e)
     return 0;
   }
   // var number_1 = JSBI.BigInt(sqrtPriceX96 *sqrtPriceX96* (1e(18))/(1e(6))/JSBI.BigInt(2) ** (JSBI.BigInt(192));
