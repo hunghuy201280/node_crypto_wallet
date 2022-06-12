3ea922c6350df8f84680f432eb2d03df1bfe88de
nxhung.hit <nxhung16102001@gmail.com>
2022-06-12 23:37:42 +0700

    update - send nft

--- src/controllers/collection.controller.ts
+++ src/controllers/collection.controller.ts
@@ -8,6 +8,7 @@ import axios from "axios";
 import log from "../utils/logger";
 import { getWeb3Instance } from "../utils/utils";
 import { ERC721Validator } from "@nibbstack/erc721-validator";
+import { TransactionType } from "../types/enums";
 const nftAbiJson = (nftAbiJsonRaw as any).default as AbiItem[];
 
 export const getNftOwner: RequestHandler = async (req, res) => {
@@ -32,13 +33,39 @@ export const getNftOwner: RequestHandler = async (req, res) => {
       try {
         const nftContract = new web3.eth.Contract(nftAbiJson, collection);
         const name = await nftContract.methods.name().call();
-        const events = await nftContract.getPastEvents("Transfer", {
+        const sendLogs = await nftContract.getPastEvents("Transfer", {
           filter: {
-            from: "0x0000000000000000000000000000000000000000",
+            from: address,
+          },
+          fromBlock: 0,
+        });
+        const receiveLogs = await nftContract.getPastEvents("Transfer", {
+          filter: {
             to: address,
           },
           fromBlock: 0,
         });
+
+        // Filter Token
+        const logs = sendLogs
+          .concat(receiveLogs)
+          .sort(
+            (a, b) =>
+              a.blockNumber - b.blockNumber ||
+              a.transactionIndex - b.transactionIndex
+          );
+
+        const owned = new Set<string>();
+
+        for (const log of logs) {
+          const { from, to, tokenId } = log.returnValues;
+
+          if (to.toLowerCase() === address.toLowerCase()) {
+            owned.add(tokenId.toString());
+          } else if (from.toLowerCase() === address.toLowerCase()) {
+            owned.delete(tokenId.toString());
+          }
+        }
 
         const list721: {
           tokenId: string;
@@ -48,19 +75,17 @@ export const getNftOwner: RequestHandler = async (req, res) => {
           description: string | undefined;
         }[] = [];
         await Promise.all(
-          events.map(async (event) => {
+          [...owned].map(async (token) => {
             try {
-              const link = await nftContract.methods
-                .tokenURI(event.returnValues.tokenId)
-                .call();
+              const link = await nftContract.methods.tokenURI(token).call();
               const data = await axios.get(link);
-              const { name, image, description, addressOwner } = data.data;
+              const { name, image, description } = data.data;
               list721.push({
                 name: name,
                 image: image,
                 description: description,
-                tokenId: event.returnValues.tokenId,
-                addressOwner: addressOwner,
+                tokenId: token,
+                addressOwner: address,
               });
             } catch (e) {}
           })
@@ -135,6 +160,62 @@ export const getErc721ValidAddress: RequestHandler = async (req, res) => {
         isValid: result.result,
       })
     );
+  } catch (err: any) {
+    console.log(err);
+    return res.status(400).send(new ErrorResponse(err.message, res.statusCode));
+  }
+};
+
+export const transferNFT: RequestHandler = async (req, res) => {
+  try {
+    const { fromAddress, toAddress, contractAddress, tokenId, fromPrivateKey } =
+      req.body;
+    if (
+      !Web3.utils.isAddress(fromAddress) ||
+      !Web3.utils.isAddress(toAddress)
+    ) {
+      throw "Address is not valid";
+    }
+    const web3 = getWeb3Instance();
+    const validator = new ERC721Validator(
+      web3,
+      "0x7afd064DaE94d73ee37d19ff2D264f5A2903bBB0"
+    );
+    const valid = await validator.basic(2, contractAddress);
+    if (!valid.result) throw "Contract address is not valid";
+
+    const nftContract = new web3.eth.Contract(nftAbiJson, contractAddress);
+    const dataField = await nftContract.methods
+      .transferFrom(fromAddress, toAddress, tokenId)
+      .encodeABI();
+
+    const data = {
+      gas: Web3.utils.toHex(100000),
+      to: contractAddress,
+      data: dataField,
+    };
+    const signedTx = await web3.eth.accounts.signTransaction(
+      data,
+      fromPrivateKey
+    );
+    const result = await web3.eth.sendSignedTransaction(
+      signedTx.rawTransaction!
+    );
+    const timestamp = (
+      await web3.eth.getBlock(result.blockNumber?.toString() ?? "")
+    ).timestamp;
+
+    return res
+      .status(200)
+      .send(
+        new SuccessResponse("Success", res.statusCode, {
+          hash: result.transactionHash,
+          from: result.from,
+          to: result.to,
+          timestamp: timestamp,
+          type: TransactionType.WITHDRAW,
+        })
+      );
   } catch (err: any) {
     console.log(err);
     return res.status(400).send(new ErrorResponse(err.message, res.statusCode));
--- src/routers/collection.route.ts
+++ src/routers/collection.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getErc721ValidAddress, getInfoCollection, getNftOwner } from "../controllers/collection.controller";
+import { getErc721ValidAddress, getInfoCollection, getNftOwner, transferNFT } from "../controllers/collection.controller";
 
 const router = Router();
 
 router.get('/owner',getNftOwner);
 router.get("/valid/:address", getErc721ValidAddress);
 router.get("/info/:address",getInfoCollection);
+router.post("/send",transferNFT);
 
 
 
